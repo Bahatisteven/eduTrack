6fb716bfd7c38cb551522562e3bdb2c8a6bb57e7
Bahatisteven <152080755+Bahatisteven@users.noreply.github.com>
2026-05-14 13:13:33 +0300

    added homeview

--- edutrack-vue/src/router/index.js
+++ edutrack-vue/src/router/index.js
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SubjectView from '../views/Subject.vue'
+//import HomeView from '../views/...'
+//import CalculatorView from '../views/...'
+//import NotesView from '../views/...'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Subject.vue'),
-    },
-  ],
+
+const routes = [
+  { path: '/',  component: SubjectView, name: 'subjects' },
+  //{ path: '/', component: HomeView, name: 'home'}
+]
+
+export default createRouter({
+  history: createWebHistory(), // uses real URL paths (not #/hash)
+  routes,
+  scrollBehavior() {
+    return { top: 0 }                                                                                                                                                                                                                                                                                                           n every navigation 
+  }
 })
-
-export default router
